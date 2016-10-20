33b3328955f173ba6d4caef84f5ee0cfe025be9c
rene.neubert <rene.neubert@aldeso.com>
2016-10-20 12:53:30 +0200

    update sonar url

--- gradle/sonar.sh
+++ gradle/sonar.sh
@@ -3,5 +3,5 @@ set -ev
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
       # ./gradlew jacocoTestReport --info
-	 sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.issuesReport.console.enable=true  -Dsonar.host.url=https://sonar.aldeso.com -Dsonar.jacoco.reportPath=build/jacoco/test.exec -Dsonar.projectVersion=$TRAVIS_BRANCH
+	 sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.issuesReport.console.enable=true  -Dsonar.host.url=http://sonar.aldeso.com:88 -Dsonar.jacoco.reportPath=build/jacoco/test.exec -Dsonar.projectVersion=$TRAVIS_BRANCH
 fi
