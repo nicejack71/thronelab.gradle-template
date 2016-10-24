8303ccee83b1b2f22c665c5d7e3c64142ed789a0
rene.neubert <rene.neubert@aldeso.com>
2016-10-24 11:15:53 +0200

    update sonar url

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -3,17 +3,50 @@
 
 git fsck --full
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew -Prelease.useLastTag=true build --info
-   #- sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.host.url=https://sonar.aldeso.com -Dsonar.projectVersion=$TRAVIS_BRANCH -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
+function strongEcho {
+  echo ""
+  echo "================ $1 ================="
+}
+
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+	strongEcho 'Build and analyze pull request'
+
+	echo '======= no deploy'
+
+    ./gradlew build check sonarqube \
+      -Dsonar.analysis.mode=issues \
+      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      -Dsonar.github.oauth=$GITHUB_TOKEN \
+      -Dsonar.host.url=https://sonar.aldeso.com \
+      -Dsonar.login=$SONAR_TOKEN
 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BRANCH build snapshot sonarqube --info
- elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PbintrayNoDryRun -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BRANCH final sonarqube --info
+  strongEcho 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+
+  ./gradlew build snapshot sonarqube \
+  -Prelease.travisci=true \
+  -PbintrayUser="${bintrayUser}" \
+  -PbintrayKey="${bintrayKey}" \
+  -Dsonar.host.url=https://sonar.aldeso.com \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.projectVersion=$TRAVIS_BRANCH
+
+
+
+
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  strongEcho 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew final sonarqube \
+  -Prelease.travisci=true \
+  -Prelease.useLastTag=true \
+  -PbintrayUser="${bintrayUser}" \
+  -PbintrayKey="${bintrayKey}" \
+  -PbintrayNoDryRun \
+  -Dsonar.host.url=https://sonar.aldeso.com \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.projectVersion=$TRAVIS_BRANCH
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
   ./gradlew -Prelease.useLastTag=true build --stacktrace
--- gradle/sonar.sh
+++ gradle/sonar.sh
@@ -21,19 +21,32 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true
       -Dsonar.host.url=https://sonar.aldeso.com \
       -Dsonar.login=$SONAR_TOKEN
 
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
+  strongEcho 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+
+  ./gradlew build snapshot sonarqube \
+  -Prelease.travisci=true \
+  -PbintrayUser="${bintrayUser}" \
+  -PbintrayKey="${bintrayKey}" \
+  -Dsonar.host.url=https://sonar.aldeso.com \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.projectVersion=$TRAVIS_BRANCH
+
+
+
+
+elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+  strongEcho 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+  ./gradlew final sonarqube \
+  -Prelease.travisci=true \
+  -Prelease.useLastTag=true \
+  -PbintrayUser="${bintrayUser}" \
+  -PbintrayKey="${bintrayKey}" \
+  -PbintrayNoDryRun \
+  -Dsonar.host.url=https://sonar.aldeso.com \
+  -Dsonar.login=$SONAR_TOKEN \
+  -Dsonar.projectVersion=$TRAVIS_BRANCH
+else
+  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
+  ./gradlew -Prelease.useLastTag=true build --stacktrace
 fi
-#
-#
-#if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-#   sonar-scanner -Dsonar.login=$SONAR_TOKEN -Dsonar.host.url=https://sonar.aldeso.com -Dsonar.projectVersion=$TRAVIS_BRANCH -Dsonar.analysis.mode=preview -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST -Dsonar.github.oauth=$GITHUB_ACCESS_TOKEN
-#
-#elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-#  echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-#  ./gradlew -Prelease.travisci=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BRANCH build snapshot sonarqube --info
-# elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-#  echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-#  ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" -PbintrayNoDryRun -Dsonar.login=$SONAR_TOKEN -Dsonar.projectVersion=$TRAVIS_BRANCH final sonarqube --info
-#else
-#  echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-#  ./gradlew -Prelease.useLastTag=true build --stacktrace
-#fi
