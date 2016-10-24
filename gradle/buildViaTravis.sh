d3485d6689e2e77764b426a83ee29087dcdbbe73
Rene Neubert <rene.neubert@aldeso.com>
2016-10-24 19:02:53 +0200

    update sonar url

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -15,36 +15,38 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_SECURE_ENV_VARS" == "true
 	echo '======= no deploy'
 
     ./gradlew build check sonarqube \
-      -Dsonar.analysis.mode=issues \
-      -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-      -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-      -Dsonar.github.oauth=$GITHUB_TOKEN \
-      -Dsonar.host.url=https://sonar.aldeso.com \
-      -Dsonar.login=$SONAR_TOKEN
+      	-Dsonar.analysis.mode=issues \
+      	-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+      	-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+      	-Dsonar.github.oauth=$GITHUB_TOKEN \
+      	-Dsonar.host.url=$SONAR_HOST_URL \
+      	-Dsonar.login=$SONAR_TOKEN
 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
-  strongEcho 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
+  	strongEcho 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
 
-  ./gradlew build snapshot sonarqube \
-  -Prelease.travisci=true \
-  -PbintrayUser="${bintrayUser}" \
-  -PbintrayKey="${bintrayKey}" \
-  -Dsonar.host.url=https://sonar.aldeso.com \
-  -Dsonar.login=$SONAR_TOKEN \
-  -Dsonar.projectVersion=$TRAVIS_BRANCH
+  	./gradlew build snapshot sonarqube \
+  		-Prelease.travisci=true \
+  		-PbintrayUser="${BINTRAY_USER}" \
+  		-PbintrayKey="${BINTRAY_KEY}" \
+  		-Dsonar.host.url=$SONAR_HOST_URL \
+  		-Dsonar.login=$SONAR_TOKEN \
+  		-Dsonar.projectVersion=$TRAVIS_BRANCH
 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
-  strongEcho 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew final sonarqube \
-  -Prelease.travisci=true \
-  -Prelease.useLastTag=true \
-  -PbintrayUser="${bintrayUser}" \
-  -PbintrayKey="${bintrayKey}" \
-  -PbintrayNoDryRun \
-  -Dsonar.host.url=https://sonar.aldeso.com \
-  -Dsonar.login=$SONAR_TOKEN \
-  -Dsonar.projectVersion=$TRAVIS_BRANCH
+  	strongEcho 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
+
+	./gradlew final sonarqube \
+  		-Prelease.travisci=true \
+  		-Prelease.useLastTag=true \
+  		-PbintrayUser="${BINTRAY_USER}" \
+  		-PbintrayKey="${BINTRAY_KEY}" \
+  		-PbintrayNoDryRun \
+  		-Dsonar.host.url=$SONAR_HOST_URL \
+  		-Dsonar.login=$SONAR_TOKEN \
+  		-Dsonar.projectVersion=$TRAVIS_BRANCH
+
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew -Prelease.useLastTag=true build --stacktrace
+  	./gradlew -Prelease.useLastTag=true build --stacktrace
 fi
